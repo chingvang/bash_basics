a649029e584009f14c61149effde35096f381b6e
Ching_Vang <chingvang76@yahoo.com>
2021-09-03 19:50:59 -0400

    two branch name update

---
+++ name_assign.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+
+# function in bash
+
+function holiday_voicemail 
+{
+   echo "Enjoy the Holiday! ${1}!"
+}
+
+function holiday_voicemail
+{
+   echo "Hope you are enjoying the holiday, ${1}!"
+}
+
+
+echo "Please enter your name"
+read Ching
+
+day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
+
+if [ $day = "Sat" ] || [ $day = "Sun" ]; then
+   holiday_voicemail $name
+else
+   holiday_voicemail $name
+fi
+
+# exercise: Add a second argument to the function 
+# and make it give a greeting based on the new 
+# argument.
