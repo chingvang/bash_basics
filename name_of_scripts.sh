307e6fab606bd2b9f4059f6e7520f9976278c6b8
Ching_Vang <chingvang76@yahoo.com>
2021-09-13 23:38:04 -0400

    4b assignment

---
+++ name_of_scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "You are happy?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Smile :)"
+else
+   echo "Still Smile :)"
+fi
+
+# here are the other string comparison operators
+# != , -n (not an empty string) , -z (an empty string)
+
+# exercise: write a script that prints whether today is
+# the weekend or not
+
+if [[ $(date +%u) -gt 5 ]]; then
+    echo 'Sorry, you cannot run this program on the weekend.'
+    exit
